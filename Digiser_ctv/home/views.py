e6799c5993283a44532fd6e42007e3f6609c46f1
Vu DaoDuc <therealfixedbot@gmail.com>
2024-06-14 20:16:44 +0700

    Add Scaffold UI for testing

--- Digiser_ctv/home/views.py
+++ Digiser_ctv/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from Digiser_ctv.services import get_all_rows
 
 # Create your views here.
 @login_required
