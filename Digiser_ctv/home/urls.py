8be50f4dec728635c503a8730bd5bc35ab7c0504
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-06-14 17:19:33 +0700

    Add homepage app

---
+++ Digiser_ctv/home/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('check/', views.check, name='check'),
+    path('insert/', views.insert, name='insert'),
+    path('support/', views.support, name='support'),
+    path('system/', views.system, name='system'),
+    path('wiki/', views.wiki, name='wiki'),
+    path('courses/', views.courses, name='courses'),
+]
--- Digiser_ctv/home/views.py
+++ Digiser_ctv/home/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'pages/home.html')
+def insert(request):
+    return render(request, 'pages/insert.html')
+def check(request):
+    return render(request, 'pages/check.html')
+def support(request):
+    return render(request, 'pages/support.html')
+def system(request):
+    return render(request, 'pages/system.html')
+def wiki(request):
+    return render(request, 'pages/wiki.html')
+def courses(request):
+    return render(request, 'pages/courses.html')
