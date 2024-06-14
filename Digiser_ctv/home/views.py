fe0cc9a39eb59bfdbbe54efc46668b89442b4782
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-06-14 23:20:15 +0700

    homepage

--- Digiser_ctv/home/views.py
+++ Digiser_ctv/home/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from Digiser_ctv.services import get_all_rows
-
+import os
 # Create your views here.
 @login_required
 def home(request):
-    return render(request, 'pages/home.html')
+    doc_name = os.getenv("DOC_LIST").split(",")[0]
+    raw_data = get_all_rows(doc_name, 1)
+    num_rows = request.GET.get('rows', 30)
+    try:
+        num_rows = int(num_rows)
+    except ValueError:
+        num_rows = 30
+    raw_data = raw_data[:num_rows]
+    context = {
+        'data': raw_data
+    }
+    return render(request, 'pages/home.html', context )
 def insert(request):
     return render(request, 'pages/insert.html')
 def check(request):
