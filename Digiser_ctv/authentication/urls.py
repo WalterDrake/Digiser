bb64776db228b6272dcbabb47b248cd5a307e93a
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-06-14 16:53:30 +0700

    Update googlesheet integration

--- Digiser_ctv/authentication/urls.py
+++ Digiser_ctv/authentication/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.LOGIN, name='login'),
     path('', views.home, name='home'),
     path('register/', views.REGISTER, name='register'),
-    path('logout/', views.LOGOUT, name='logout')
+    path('logout/', views.LOGOUT, name='logout'),
+    path('test/', views.Test, name='test')
 ]
--- Digiser_ctv/authentication/views.py
+++ Digiser_ctv/authentication/views.py
@@ -1,9 +1,13 @@
+import os
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from .forms import CustomUserCreationForm
+from Digiser_ctv.services import add_row, count_row
+
 # Create your views here.
+
 
 @csrf_protect
 def REGISTER(request):
@@ -14,13 +18,19 @@ def REGISTER(request):
             # Set the username to email if necessary
             user.username = form.cleaned_data.get('email').split("@")[0]
             user.save()  # Save the user instance
-            print(user.id)
+            doc_name = os.getenv("DOC_LIST").split(",")[0]
+            cnt = count_row(doc_name, 0)
+            user_dict = {
+                "ID": "DGS" + str(cnt),
+                "password": form.cleaned_data.get('password2'),
+                "gmail": user.email
+            }
+            add_row(doc_name, 0, cnt, user_dict)
             login(request, user)
             return redirect('home')
     else:
         form = CustomUserCreationForm()
     return render(request, 'register.html')
-
 
 
 @csrf_protect
@@ -32,7 +42,8 @@ def LOGIN(request):
         print(user)
         if user is not None:
             login(request, user)
-            return redirect('home')  # Replace 'home' with the name of your home URL pattern
+            # Replace 'home' with the name of your home URL pattern
+            return redirect('home')
         else:
             messages.error(request, 'Invalid phone number or password.')
 
@@ -46,3 +57,4 @@ def LOGOUT(request):
 
 def home(request):
     return render(request, 'home.html')
+
