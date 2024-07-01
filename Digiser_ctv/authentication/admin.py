fc10ac07d51d367eaeb6e8b74b74d3637877e6a0
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-07-01 22:12:20 +0700

    add field user

--- Digiser_ctv/authentication/admin.py
+++ Digiser_ctv/authentication/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
 
@@ -9,18 +10,23 @@ class CustomUserAdmin(UserAdmin):
                     'is_staff', 'is_superuser', 'last_login',)
     list_filter = ('is_active', 'is_staff', 'is_superuser')
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password','phone_no', 'is_verified')}),
+        (None, {'fields': ('username', 'email', 'password', 'phone_no', 'is_verified', 'full_name',
+         'address', 'qualification', 'identification', 'identification_address', 'note', 'code_ctv', 'role'
+                           )}),
         ('Permissions', {'fields': ('is_staff', 'is_active',
          'is_superuser', 'groups', 'user_permissions')}),
-        ('Dates', {'fields': ('last_login', 'date_joined')})
+        ('Bank', {'fields': ('account_number',
+         'bank_name', 'branch', 'owner', 'code_bank')}),
+        ('Dates', {'fields': ('last_login', 'date_joined', 'birthday',)})
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'phone_no','password1', 'password2', 'is_staff', 'is_active',)}
+            'fields': ('username', 'email', 'phone_no', 'password1', 'password2', 'is_staff', 'is_active',)}
          ),
     )
     search_fields = ('email',)
     ordering = ('email',)
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
