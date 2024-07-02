18489fec66c984302388094133cb6ea7881cb5ca
Vu DaoDuc <therealfixedbot@gmail.com>
2024-07-02 17:10:06 +0700

    Refactor: Clean up code and separate logic

--- Digiser_ctv/authentication/views.py
+++ Digiser_ctv/authentication/views.py
@@ -3,74 +3,80 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
-from .forms import CustomUserCreationForm
-from Digiser_ctv.services import add_row, count_row, find_id_with_username, send_zalo_oa_message_to_client
 from django.contrib.auth.models import Group
-# Create your views here.
+from .forms import CustomUserCreationForm
+from Digiser_ctv.services import update_row, count_row
 
 @csrf_protect
 def REGISTER(request):
-    if request.method == "POST":
-        form = CustomUserCreationForm(request.POST)
-        if form.is_valid():
-            doc_name = os.getenv("DOC_LIST").split(",")[0]
-            cnt = count_row(doc_name, 0)
-            user = form.save(commit=False)
-            # Set the username to email if necessary
-            user.username = form.cleaned_data.get('email').split("@")[0]
-            user.row = cnt
-            user.save()  # Save the user instance
-            group = Group.objects.get(name='CTV')
-            group.user_set.add(user)
-            user_dict = {
-                "ID": "undefined",
-                "password": form.cleaned_data.get('password2'),
-                "gmail": user.email,
-                "phone": user.phone_no,
-                "birthday": "undefined",
-                "full name": "undefined",
-                "address": "undefined",
-                "qualification": "undefined",
-                "identification": "undefined",
-                "identification address": "undefined",
-                "note": "undefined",
-                "role": "undefined",
-                "account number": "undefined",
-                "bank name": "undefined",
-                "branch": "undefined",
-                "owner": "undefined",
-                "code bank": "undefined",
-            }
-            print(add_row(doc_name, 0, cnt, user_dict))
-            # user_id = find_id_with_username(form.cleaned_data.get('username_zalo'))
-            # if user_id['data'] != 'none':
-            #     send_zalo_oa_message_to_client(user_id['data'], f"""Chúc mừng {request.POST.get('username_zalo')}. Bạn đã đăng ký tài khoản CTV thành công tại Digiser. Đăng nhập với số điện thoại {form.cleaned_data.get('phone_no')} và mật khẩu là {form.cleaned_data.get('password2')}""")
-            request.session['user_id'] = user.id
-            login(request, user)
-            return redirect('/home')
-    else:
-        form = CustomUserCreationForm()
-    return render(request, 'register.html')
+    if request.method != "POST":
+        return render(request, 'register.html', {'form': CustomUserCreationForm()})
 
+    form = CustomUserCreationForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'register.html', {'form': form})
+
+    user = create_user(form)
+    add_user_to_group(user, 'CTV')
+    update_user_data(user, form)
+    login_and_redirect(request, user)
+    return redirect('/home')
+
+def create_user(form):
+    user = form.save(commit=False)
+    user.username = form.cleaned_data.get('email').split("@")[0]
+    user.row = count_row(os.getenv("DOC_LIST").split(",")[0], 0)
+    user.save()
+    return user
+
+def add_user_to_group(user, group_name):
+    group = Group.objects.get(name=group_name)
+    group.user_set.add(user)
+
+def update_user_data(user, form):
+    doc_name = os.getenv("DOC_LIST").split(",")[0]
+    user_dict = {
+        "ID": "undefined",
+        "password": form.cleaned_data.get('password2'),
+        "gmail": user.email,
+        "phone": user.phone_no,
+        "birthday": "undefined",
+        "full name": "undefined",
+        "address": "undefined",
+        "qualification": "undefined",
+        "identification": "undefined",
+        "identification address": "undefined",
+        "note": "undefined",
+        "role": "undefined",
+        "account number": "undefined",
+        "bank name": "undefined",
+        "branch": "undefined",
+        "owner": "undefined",
+        "code bank": "undefined",
+    }
+    update_row(doc_name, 0, user.row, user_dict)
+
+def login_and_redirect(request, user):
+    request.session['user_id'] = user.id
+    login(request, user)
 
 @csrf_protect
 def LOGIN(request):
-    if request.method == 'POST':
-        phone_no = request.POST.get('phone_no')
-        password = request.POST.get('password')
-        user = authenticate(request, username=phone_no, password=password)
-        if user is not None:
-            print(user.is_verified)
-            request.session['user_id'] = user.id
-            login(request, user) 
-            # Replace 'home' with the name of your home URL pattern
-            return redirect('home')
-        else:
-            messages.error(request, 'Invalid phone number or password.')
+    if request.method != 'POST':
+        return render(request, 'login.html')
 
-    return render(request, 'login.html')
+    phone_no = request.POST.get('phone_no')
+    password = request.POST.get('password')
+    user = authenticate(request, username=phone_no, password=password)
+
+    if user is not None:
+        request.session['user_id'] = user.id
+        login(request, user)
+        return redirect('home')
+    else:
+        messages.error(request, 'Invalid phone number or password.')
+        return render(request, 'login.html')
 
 def LOGOUT(request):
     logout(request)
     return redirect('/home')
-
--- Digiser_ctv/home/views.py
+++ Digiser_ctv/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from Digiser_ctv.services import get_all_rows, add_row, count_row
+from Digiser_ctv.services import get_all_rows, update_row, count_row
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from .forms import UploadFileForm
@@ -9,173 +9,163 @@ import pandas as pd
 import os
 from authentication.models import CustomUser
 
-
 @csrf_exempt
 def upload_import(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            uploaded_file = request.FILES['file']
-            # Use openpyxl engine to read Excel file
-            user_df = pd.read_excel(uploaded_file, engine='openpyxl')
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Invalid request'}, status=400)
+    
+    form = UploadFileForm(request.POST, request.FILES)
+    if not form.is_valid():
+        return JsonResponse({'error': 'Form is not valid'}, status=400)
+    
+    user_df = pd.read_excel(request.FILES['file'], engine='openpyxl')
+    doc_name = os.getenv("DOC_LIST").split(",")[0]
+    
+    for _, user in user_df.iterrows():
+        process_user(user, doc_name)
+    
+    return JsonResponse({'message': 'File uploaded successfully'})
 
-            for _, user in user_df.iterrows():
-                form = CustomUserCreationForm({"username": user['username'],
-                                               "password1": user['password1'],
-                                               "password2": user['password2'],
-                                               "email": user['email'],
-                                               "phone_no": user['phone_no']})
-
-                doc_name = os.getenv("DOC_LIST").split(",")[0]
-                if form.is_valid():
-                    user = form.save(commit=False)
-                    user.username = form.cleaned_data.get('email').split("@")[0]
-                    user.save()  # Save the user instance
-                    cnt = count_row(doc_name, 0)
-                    user_dict = {
-                        "ID": "",
-                        "password": form.cleaned_data.get('password2'),
-                        "gmail": user.email
-                    }
-                    add_row(doc_name, 0, cnt, user_dict)
-
-            return JsonResponse({'message': 'File uploaded successfully'})
-        else:
-            return JsonResponse({'error': 'Form is not valid'}, status=400)
-    return JsonResponse({'error': 'Invalid request'}, status=400)
-
+def process_user(user_data, doc_name):
+    form = CustomUserCreationForm({
+        "username": user_data['username'],
+        "password1": user_data['password1'],
+        "password2": user_data['password2'],
+        "email": user_data['email'],
+        "phone_no": user_data['phone_no']
+    })
+    
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.username = form.cleaned_data.get('email').split("@")[0]
+        user.save()
+        
+        cnt = count_row(doc_name, 0)
+        user_dict = {
+            "ID": "",
+            "password": form.cleaned_data.get('password2'),
+            "gmail": user.email
+        }
+        update_row(doc_name, 0, cnt, user_dict)
 
 @login_required
 def home(request):
     if request.method == 'POST':
-        if (checkManager(request.user)):
-            email = request.POST.get('gmail')
-            doc_name = os.getenv("DOC_LIST").split(",")[0]
-            cnt = count_row(doc_name, 0)
-            user = get_object_or_404(CustomUser, email=email)
-            user.is_verified = True
-            user.role = 'CTV'
-            user.code_ctv = 'NV' + str(cnt-1)
-            user.save()
-            return render(request, 'pages/home_manager.html')
+        return handle_post_request(request)
     elif request.method == 'GET':
+        return handle_get_request(request)
+
+def handle_post_request(request):
+    if checkManager(request.user):
+        email = request.POST.get('gmail')
         doc_name = os.getenv("DOC_LIST").split(",")[0]
-        raw_data = get_all_rows(doc_name, 1)
-        num_rows = request.GET.get('rows', 10)
-        try:
-            num_rows = int(num_rows)
-        except ValueError:
-            num_rows = 10
-        raw_data = raw_data[:num_rows]
-        context = {
-            'data': raw_data
-        }
-        user_id = request.session.get('user_id')
-        user = get_object_or_404(CustomUser, id=user_id)
+        cnt = count_row(doc_name, 0)
+        user = get_object_or_404(CustomUser, email=email)
+        user.is_verified = True
+        user.role = 'CTV'
+        user.code_ctv = f'NV{cnt-1}'
+        user.save()
+    return render(request, 'pages/home_manager.html')
 
-        if checkManager(user):
-            return render(request, 'pages/home_manager.html', context)
+def handle_get_request(request):
+    doc_name = os.getenv("DOC_LIST").split(",")[0]
+    raw_data = get_all_rows(doc_name, 1)
+    num_rows = int(request.GET.get('rows', 10))
+    context = {'data': raw_data[:num_rows]}
+    
+    user_id = request.session.get('user_id')
+    user = get_object_or_404(CustomUser, id=user_id)
+    
+    if checkManager(user):
+        return render(request, 'pages/home_manager.html', context)
+    return render(request, 'pages/home_ctv.html', context)
 
-        return render(request, 'pages/home_ctv.html', context)
-
-
+@login_required
 def insert(request):
     return render(request, 'pages/insert.html')
 
-
+@login_required
 def check(request):
     return render(request, 'pages/check.html')
-
-
+@login_required
 def support(request):
     return render(request, 'pages/support.html')
-
-
+@login_required
 def system(request):
     return render(request, 'pages/system.html')
 
-
+@login_required
 def wiki(request):
     return render(request, 'pages/wiki.html')
 
-
+@login_required
 def courses(request):
     return render(request, 'pages/courses.html')
 
-
+@login_required
 def info(request):
     user_id = request.session.get('user_id')
     user = get_object_or_404(CustomUser, id=user_id)
+    
     if request.method == 'POST':
-        form = CustomUserChangeForm(request.POST, instance=request.user)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.save()
-            doc_name = os.getenv("DOC_LIST").split(",")[0]
-            cnt = count_row(doc_name, 0)
-            print(cnt)
-            user_dict = {
-                "ID" : user.code_ctv,
-                "gmail": user.email,
-                "password": user.password, # fixed hash password
-                "phone": user.phone_no,
-                "birthday": user.birthday,
-                "full name": user.full_name,
-                "address": user.address,
-                "qualification": user.qualification,
-                "identification": user.identification,
-                "identification address": user.identification_address,
-                "note": user.note,
-                "role": user.role,
-                "account number": user.account_number,
-                "bank name": user.bank_name,
-                "branch": user.branch,
-                "owner": user.owner,
-                "code bank": user.code_bank
-            }
-            add_row(doc_name, 0, user.row, user_dict)
-            context = {
-                "code_ctv": user.code_ctv,
-                "phone_no": user.phone_no,
-                "birthday": user.birthday,
-                "full_name": user.full_name,
-                "address": user.address,
-                "qualification": user.qualification,
-                "identification": user.identification,
-                "identification_address": user.identification_address,
-                "note": user.note,
-                "account_number": user.account_number,
-                "bank_name": user.bank_name,
-                "branch": user.branch,
-                "owner": user.owner,
-                "code_bank": user.code_bank
-            }
-            return render(request, 'pages/info.html', context)
-    elif user.is_verified == True:
-        user_id = request.session.get('user_id')
-        user = get_object_or_404(CustomUser, id=user_id)
-        context = {
-            "code_ctv": user.code_ctv,
-            "phone_no": user.phone_no,
-            "birthday": user.birthday,
-            "full_name": user.full_name,
-            "address": user.address,
-            "qualification": user.qualification,
-            "identification": user.identification,
-            "identification_address": user.identification_address,
-            "note": user.note,
-            "account_number": user.account_number,
-            "bank_name": user.bank_name,
-            "branch": user.branch,
-            "owner": user.owner,
-            "code_bank": user.code_bank
-        }
-        return render(request, 'pages/info.html', context)
+        return handle_info_post(request, user)
+    elif user.is_verified:
+        return handle_info_get(request, user)
     else:
-        return render(request, 'pages/home_ctv.html')
+        handle_get_request()
 
+def handle_info_post(request, user):
+    form = CustomUserChangeForm(request.POST, instance=request.user)
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.save()
+        update_user_info(user)
+        return render(request, 'pages/info.html', get_user_context(user))
+    return render(request, 'pages/info.html')
+
+def handle_info_get(request, user):
+    return render(request, 'pages/info.html', get_user_context(user))
+
+def update_user_info(user):
+    doc_name = os.getenv("DOC_LIST").split(",")[0]
+    user_dict = {
+        "ID": user.code_ctv,
+        "gmail": user.email,
+        "password": user.password,
+        "phone": user.phone_no,
+        "birthday": user.birthday,
+        "full name": user.full_name,
+        "address": user.address,
+        "qualification": user.qualification,
+        "identification": user.identification,
+        "identification address": user.identification_address,
+        "note": user.note,
+        "role": user.role,
+        "account number": user.account_number,
+        "bank name": user.bank_name,
+        "branch": user.branch,
+        "owner": user.owner,
+        "code bank": user.code_bank
+    }
+    update_row(doc_name, 0, user.row, user_dict)
+
+def get_user_context(user):
+    return {
+        "code_ctv": user.code_ctv,
+        "phone_no": user.phone_no,
+        "birthday": user.birthday,
+        "full_name": user.full_name,
+        "address": user.address,
+        "qualification": user.qualification,
+        "identification": user.identification,
+        "identification_address": user.identification_address,
+        "note": user.note,
+        "account_number": user.account_number,
+        "bank_name": user.bank_name,
+        "branch": user.branch,
+        "owner": user.owner,
+        "code_bank": user.code_bank
+    }
 
 def checkManager(user):
-    if user.groups.filter(name='Manager').exists():
-        return True
-    return False
+    return user.groups.filter(name='Manager').exists()
