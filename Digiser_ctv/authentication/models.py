d43d46cdb4d741062e73c90ef9d834df4958a345
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-07-30 20:33:32 +0700

    adjust model

--- Digiser_ctv/authentication/models.py
+++ Digiser_ctv/authentication/models.py
@@ -1,16 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.utils.translation import gettext as _
-import datetime
-# Create your models here.
-
+from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
-
+# Create your models here.
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
-    phone_no = models.CharField(_('phone number'), max_length=20, unique=True)
+    phone_no = models.CharField(
+        _('phone number'), max_length=15, unique=True, primary_key=True)
     is_verified = models.BooleanField(_('is verified'), default=False)
     full_name = models.CharField(_('full name'), blank=True, max_length=20)
     birthday = models.DateField(_('birthday'), blank=True, null=True)
@@ -28,11 +26,51 @@ class CustomUser(AbstractUser):
     branch = models.CharField(_('branch'), max_length=20, blank=True)
     owner = models.CharField(_('owner'), max_length=20, blank=True)
     code_bank = models.CharField(_('code bank'), max_length=10, blank=True)
-    code_ctv = models.CharField(_('code ctv'), max_length=10, blank=True, unique=True)
+    code = models.CharField(
+        _('code'), max_length=10, blank=True, unique=True)
     role = models.CharField(_('role'), max_length=10, blank=True)
-    row = models.IntegerField(_('row'), blank=True, null=True)
+    _STATUSES = (
+        ("AC", "Working"),
+        ("NA", "Quit"),
+    )
+    status = models.CharField(
+        _('status'), max_length=2, choices=_STATUSES, blank=True)
+    _SKILLS = (
+        ('Civil, Online, A3', 'Civil, Online, A3'),
+        ('Civil, Offline, A3', 'Civil, Offline, A3'),
+        ('Civil, Online, A4', 'Civil, Online, A4'),
+        ('Civil, Offline, A4', 'Civil, Offline, A4'),
+        ('VBHC, Online, A3', 'VBHC, Online, A3'),
+        ('VBHC, Offline, A3', 'VBHC, Offline, A3'),
+        ('VBHC, Online, A4', 'VBHC, Online, A4'),
+        ('VBHC, Offline, A4', 'VBHC, Offline, A4'),
+        ('Merging, Online, A3', 'Merging, Online, A3'),
+        ('Merging, Offline, A3', 'Merging, Offline, A3'),
+        ('Merging, Online, A4', 'Merging, Online, A4'),
+        ('Merging, Offline, A4', 'Merging, Offline, A4'),
+        ('Naming, Online, A3', 'Naming, Online, A3'),
+        ('Naming, Offline, A3', 'Naming, Offline, A3'),
+        ('Naming, Online, A4', 'Naming, Online, A4'),
+        ('Naming, Offline, A4', 'Naming, Offline, A4')
+    )
+    skill = models.CharField(_('skill'), max_length=30,
+                             choices=_SKILLS, blank=True)
+    _STYLE = (
+        ("AD", "Admin"),
+        ("CTV", "CTV"),
+        ("MGR", "Manager"),
+        ("SP", "Support"),
+    )
+    style = models.CharField(
+        _('style'), max_length=3, choices=_STYLE, blank=True)
+    updated_time = models.DateTimeField(
+        _('update time'), blank=True, null=True)
+    updated_user = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, blank=True, null=True, related_name='updated_by')
+
     USERNAME_FIELD = 'phone_no'
     REQUIRED_FIELDS = ['username', 'email']
+
     objects = CustomUserManager()
 
     def __str__(self):
