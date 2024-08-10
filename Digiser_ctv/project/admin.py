47d94ee0678e858719cfada39abe576a2950e3a3
U-WALTERDRAKE\dat12 <dat123mall@gmail.com>
2024-08-10 22:48:05 +0700

    add admin site

--- Digiser_ctv/project/admin.py
+++ Digiser_ctv/project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Project, Package, Package_detail
+from .models.model1 import Project, Package, Package_detail, Document
+from .models.model2 import Birth_Certificate_Document
 # Register your models here.
-admin.site.register([Project, Package, Package_detail])
+admin.site.register([Project, Package, Package_detail, Document, Birth_Certificate_Document])
