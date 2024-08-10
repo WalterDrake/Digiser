1a0deacc94835840f91b7775ee45792fadce5f48
F1xedbot <therealfixedbot@gmail.com>
2024-08-11 01:43:42 +0700

    Remove register path and user creation function in views

--- Digiser_ctv/authentication/urls.py
+++ Digiser_ctv/authentication/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LOGIN, name='login'),
-    path('register/', views.REGISTER, name='register'),
     path('logout/', views.LOGOUT, name='logout'),
 ]
--- Digiser_ctv/authentication/views.py
+++ Digiser_ctv/authentication/views.py
@@ -4,29 +4,6 @@ from django.contrib.auth import login, authenticate, logout
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from django.contrib.auth.models import Group
-from .forms import CustomUserCreationForm
-
-@csrf_protect
-def REGISTER(request):
-    if request.method != "POST":
-        return render(request, 'auth/register.html', {'form': CustomUserCreationForm()})
-
-    form = CustomUserCreationForm(request.POST)
-    if not form.is_valid():
-        return render(request, 'auth/register.html', {'form': form})
-
-    user = create_user(form)
-    add_user_to_group(user, 'CTV')
-    login_and_redirect(request, user)
-    return redirect('/home')
-
-
-def create_user(form):
-    user = form.save(commit=False)
-    user.username = form.cleaned_data.get('email').split("@")[0]
-    user.save()
-    return user
-
 
 def add_user_to_group(user, group_name):
     group = Group.objects.get(name=group_name)
