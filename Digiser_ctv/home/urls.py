621045af6021c28e2132c2db120c2929e9c4b95d
marco <longhkt10@gmail.com>
2024-09-05 09:52:55 +0700

    Redirect Base Page to Dash Board

--- Digiser_ctv/home/urls.py
+++ Digiser_ctv/home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('check/', views.check, name='check'),
     path('insert/', views.insert, name='insert'),
