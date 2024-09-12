7c6fcd12259f228fbd16c4610f5e9c7106660943
F1xedbot <therealfixedbot@gmail.com>
2024-09-13 01:27:32 +0700

    Temporary changed export path to export for user only (unavailable for packages-based)

--- Digiser_ctv/project/urls.py
+++ Digiser_ctv/project/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('<str:id>/<int:index>', views.input_redirect, name=""),
-    path('export/<str:id>', views.export_package, name=""),
+    path('export/user/', views.export_package, name=""),
+    path('redirect', views.package_redirect, name=""),
 ]
