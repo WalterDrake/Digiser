cc4afd05f2e133019e214c4a111cbc2bf293bdcd
Walter Drake <dat123mall@gmail.com>
2024-09-26 18:41:33 +0700

    move function from view to utils

--- Digiser_ctv/project/views.py
+++ Digiser_ctv/project/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404, HttpResponse, JsonResponse
 from django.utils.html import escape
-from django.contrib.auth.decorators import login_required
-import re
-from .models.model2 import Birth_Certificate_Document
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .utils import *
+from .models.model2 import *
 from .models.model1 import Document, Package_detail, Package
 from authentication.models import CustomUser
-from django.forms.models import model_to_dict
-from datetime import datetime, date
-from django.contrib.auth.decorators import user_passes_test
-from .utils import birth_certificate_documents_to_df
-import hashlib
+import hashlib, zipfile, io
 from django.db.models import Q
-import io
-import zipfile
 import pandas as pd
-from django.db.models.fields import Field
 
 @login_required
 def form_redirect(request, **kwargs):
@@ -48,8 +41,6 @@ def form_redirect(request, **kwargs):
     else:
         raise Http404("Document type not supported")
     
-def is_superuser(user):
-    return user.is_superuser
 
 def package_redirect(request):
     if request.method == 'POST':
@@ -141,9 +132,6 @@ def export_package(request):
     return JsonResponse({'status': 'error', 'message': 'Only POST requests are allowed'})
         
 
-def is_valid_package_id(package_id):
-    return isinstance(package_id, str) and re.match(r'^[a-zA-Z0-9_-]+$', package_id)
-
 
 def get_valid_index(index_str, documents_count):
     try:
@@ -153,36 +141,19 @@ def get_valid_index(index_str, documents_count):
     except (ValueError, TypeError):
         pass
     raise Http404("Invalid document index")
-
+    
 
 def birth_certificate_document(request, document, user, role, package_detail_name=None):
     date_fields = ['ngayDangKy', 'nksNgaySinh', 'meNgaySinh', 'chaNgaySinh', 'nycNgayCapGiayToTuyThan']
-    def prepare_form_data(form_instance=None, lock=False):
-        document_name = form_instance.document.document_name.split('.') if form_instance else document.document_name.split('.')
-        common_data = {
-            'pdf_path': form_instance.document.document_path if form_instance else document.document_path,
-            'lock': lock,
-            'total_fields': form_instance.document.package_name.total_fields if form_instance else document.package_name.total_fields,
-            'entered_tickets': form_instance.document.package_name.entered_tickets if form_instance else document.package_name.entered_tickets or 0,
-            'total_real_tickets': form_instance.document.package_name.total_real_tickets if form_instance else document.package_name.total_real_tickets,
-            'so': document_name[4] if len(document_name) > 0 else "",
-            'quyenSo': document_name[2] if len(document_name) > 0 else ""
-        }
-        if form_instance:
-            form_data = handle_date_fields(model_to_dict(form_instance), date_fields)
-            form_data.update(common_data)
-        else:
-            form_data = common_data
-        return form_data
 
+    # Handling POST request
     if request.method == 'POST':
         form_data = getattr(request, 'form_data', None)
         error_list = getattr(request, 'error_list', None)
         
         if error_list:
             form_data.update(get_form_data_choices())
-            form_data.update(prepare_form_data())
-            form_data = handle_date_fields(form_data, date_fields)
+            form_data.update(prepare_form_data(document, date_fields=date_fields))
             return render(request, 'pages/form.html', {'form': form_data, 'form_type': 'birth_cert', 'error_list': error_list})
         
         form_data['executor'] = user
@@ -192,27 +163,16 @@ def birth_certificate_document(request, document, user, role, package_detail_nam
         if role == 'inserter' and package_detail_name:
             package = Package.objects.filter(package_name=package_detail_name).first()
             if package:
-                entered_tickets_count = Birth_Certificate_Document.objects.filter(
-                    document__package_name=package, executor=user).count()
-                package.entered_tickets = entered_tickets_count
-                package.save()  
-                all_fields = [f.name for f in form_instance._meta.get_fields() if isinstance(f, Field) and not f.auto_created]
-                non_empty_field_count = sum(1 for field in all_fields if getattr(form_instance, field))
-                if non_empty_field_count >= form_instance.document.fields:
-                    form_instance.document.status_insert = 'Đã nhập'
-                    form_instance.document.save() 
-                if entered_tickets_count == package.total_real_tickets:
-                    package_detail_name.insert_status = 'Hoàn thành nhập'
-                    package_detail_name.save()
+                handle_package_status(package, user, form_instance)
+
         form_data.update(get_form_data_choices())
-        form_data.update(prepare_form_data(form_instance))
+        form_data.update(prepare_form_data(document, form_instance=form_instance, date_fields=date_fields))
         return render(request, 'pages/form.html', {'form': form_data, 'form_type': 'birth_cert', 'error_list': None})
 
-    # Handling non-POST (GET) request
+    # Handling GET request
     lock = False
     form_instance = None
 
-    # Fetching form instance based on role and status
     if role == 'inserter':
         form_instance = Birth_Certificate_Document.objects.filter(document=document).first()
         if form_instance and form_instance.document.status_insert == 'Đã nhập':
@@ -228,26 +188,7 @@ def birth_certificate_document(request, document, user, role, package_detail_nam
             lock = True
 
     form_data = get_form_data_choices()
-    form_data.update(prepare_form_data(form_instance, lock))
+    form_data.update(prepare_form_data(document, form_instance=form_instance, lock=lock, date_fields=date_fields))
     return render(request, 'pages/form.html', {'form': form_data, 'form_type': 'birth_cert'})
-    
 
 
-def get_form_data_choices():
-        return {
-            'DanToc': Birth_Certificate_Document._meta.get_field('nksDanToc').choices,
-            'GioiTinh': Birth_Certificate_Document._meta.get_field('nksGioiTinh').choices,
-            'QuocTich': Birth_Certificate_Document._meta.get_field('nksQuocTich').choices,
-            'LoaiDangKy': Birth_Certificate_Document._meta.get_field('loaiDangKy').choices,
-            'LoaiCuTru': Birth_Certificate_Document._meta.get_field('meLoaiCuTru').choices,
-            'LoaiGiayToTuyThan': Birth_Certificate_Document._meta.get_field('meLoaiGiayToTuyThan').choices,
-            'LoaiKhaiSinh': Birth_Certificate_Document._meta.get_field('nksLoaiKhaiSinh').choices,
-        }
-
-
-def handle_date_fields(data, fields):
-    for field in fields:
-        if field in data:
-            if isinstance(data[field], date):
-                data[field] = data[field].strftime('%d/%m/%Y')
-    return data
